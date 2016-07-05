8f77a3fe6203c0c2ddae0b291679f470d664edfc
Matt T <code.matthew.thompson@gmail.com>
2016-07-05 17:42:42 -0400

    adds form with basic validation

--- angular2_frontend/app/addjob_component/addjob.component.js
+++ angular2_frontend/app/addjob_component/addjob.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core'], function(exports_1, context_1) {
+System.register(['@angular/core', '@angular/common'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,21 +10,28 @@ System.register(['@angular/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, common_1;
     var AddJobComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (common_1_1) {
+                common_1 = common_1_1;
             }],
         execute: function() {
             AddJobComponent = (function () {
                 function AddJobComponent() {
+                    this.form = new common_1.ControlGroup({
+                        name: new common_1.Control('', common_1.Validators.required),
+                        description: new common_1.Control('', common_1.Validators.required)
+                    });
                 }
                 AddJobComponent = __decorate([
                     core_1.Component({
                         selector: 'addnew',
-                        template: "\n    Add Jerb\n  "
+                        templateUrl: 'app/addjob_component/addjob.component.html'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AddJobComponent);
--- angular2_frontend/app/addjob_component/addjob.component.ts
+++ angular2_frontend/app/addjob_component/addjob.component.ts
@@ -1,11 +1,14 @@
 import {Component} from '@angular/core'
+import {ControlGroup, Control, Validators} from '@angular/common';
 
 @Component({
   selector: 'addnew',
-  template: `
-    Add Jerb
-  `
+  templateUrl: 'app/addjob_component/addjob.component.html'
 })
 export class AddJobComponent {
+  form = new ControlGroup({
+    name: new Control('',Validators.required),
+    description: new Control('',Validators.required)
+  });
 
 }
